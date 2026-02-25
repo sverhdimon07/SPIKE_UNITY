fde62fa8513831003060ab206d1edf3a5f23c5b3
sverhdimon07 <86204314+sverhdimon07@users.noreply.github.com>
2026-02-25 05:24:05 +0300

    Commit 3

--- Assets/!GameFiles/Scripts/Character/CharacterControllerNew.cs
+++ Assets/!GameFiles/Scripts/Character/CharacterControllerNew.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterControllerNew : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
